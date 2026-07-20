e9d0b25b13ae8970b455acb3219b819e0b909f24
Rohit Vishwakarma <wishrohitv@proton.me>
2026-07-20 15:23:02 +0530

    Feat: Add exponential backoff for ratelimiting

--- backend/middlewares/rate_limiter.py
+++ backend/middlewares/rate_limiter.py
@@ -10,19 +10,25 @@ def rate_limiter_middleware(
     Rate limiter middleware that limits the number of requests to a given endpoint.
     Defaults to 5 requests per 60 seconds.
     Args:
-        endpoint (str): The endpoint to rate limit.
+        route (RouteAccess): The endpoint to rate limit.
         limit (int): The maximum number of requests allowed within the period.
         period (int): The time period in seconds for the rate limit.
+        exponential (bool): Whether to use exponential backoff for rate limiting.
     """
 
     def route_funtion(func):
         def wrapper(*args, **kwargs):
             key = f"rate_limit_endpoint:{route.route_name}:{request.remote_addr}"
             count = redis_client.incr(key)
+            ttl = redis_client.ttl(key)
             if count == 1:
                 redis_client.expire(key, period)
             if int(count) > limit:
-                raise RateLimitExceededError()
+                if exponential:
+                    redis_client.expire(key, ttl + period)
+                raise RateLimitExceededError(
+                    f"Rate limit exceeded for endpoint {route.route_name}, try after {ttl} seconds"
+                )
             return func(*args, **kwargs)
 
         return wrapper
