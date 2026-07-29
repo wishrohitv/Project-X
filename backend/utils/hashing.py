60380fb83bf0a93863ae5a53d3ab09688015cc00
Rohit Vishwakarma <wishrohitv@proton.me>
2026-07-29 13:10:17 +0530

    Ref: Optimize code

--- backend/utils/hashing.py
+++ backend/utils/hashing.py
@@ -6,10 +6,7 @@ from modules import bcrypt
 # hashed = bcrypt.hashpw(password, bcrypt.gensalt())
 # Check that an unhashed password matches one that has previously been hashed
 def match_password(password: bytes, hashed: bytes) -> bool:
-    if bcrypt.checkpw(password, hashed):
-        return True
-    else:
-        return False
+    return bcrypt.checkpw(password, hashed)
 
 
 def return_hashed_bytes(password):
