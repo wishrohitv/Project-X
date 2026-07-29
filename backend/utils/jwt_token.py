76b2bc8f4f62df987c50d1153b1193d092689c3f
Rohit Vishwakarma <wishrohitv@proton.me>
2026-07-29 12:12:51 +0530

    Ref: Add hask key through argument parameter

--- backend/utils/jwt_token.py
+++ backend/utils/jwt_token.py
@@ -4,25 +4,26 @@ from modules import datetime, jwt, timedelta
 from settings import Settings
 
 
-def decode_jwt_token(client_token):
+def decode_jwt_token(client_token, hash_key):
     """
     Decodes a JWT token using the application's hash key.
     exception should be handled by the caller.
     Args:
         client_token (str): The JWT token to decode.
+        hash_key (str): The hash key to use for decoding.
 
     Returns:
         dict: The decoded token payload.
     """
-    data = jwt.decode(jwt=client_token, key=Settings.JWT_HASH_KEY, algorithms="HS256")
+    data = jwt.decode(jwt=client_token, key=hash_key, algorithms="HS256")
     return data
 
 
-def generate_jwt_token(user_data: dict[str, Any], expire_in_minute: int):
+def generate_jwt_token(user_data: dict[str, Any], hash_key: str, expire_in_minute: int):
     current_time = datetime.now() + timedelta(minutes=expire_in_minute)
     unix_timestamp = current_time.timestamp()  # expiry time
 
     payload = {"payload": user_data, "exp": int(unix_timestamp)}
 
-    token = jwt.encode(payload, Settings.JWT_HASH_KEY, algorithm="HS256")
+    token = jwt.encode(payload, hash_key, algorithm="HS256")
     return token
